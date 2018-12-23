87bc788f1ebef765acaeac0f73faf44704537b8e
Peter Erdodi <peter.erdodi@exputec.com>
2018-12-23 22:01:21 +0100

    Fallback to 404

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -46,6 +46,10 @@ class Handler extends ExceptionHandler
      */
     public function render($request, Exception $exception)
     {
+        if ($exception instanceof ModelNotFoundException && $request->isJson()) {
+            return Route::respondWithRoute('api.fallback.404');
+        }
+
         return parent::render($request, $exception);
     }
 }
